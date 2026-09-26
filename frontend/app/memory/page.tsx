cabc1a8cc337a32da3c678587d685fe60f731365
Ayan <ayangoku753@gmail.com>
2026-09-26 20:16:48 +0500

    feat: add memory management route

---
+++ frontend/app/memory/page.tsx
@@ -0,0 +1,11 @@
+import AppShell from "@/components/layout/AppShell";
+import MemoryWorkspace from "@/components/memory/MemoryWorkspace";
+
+
+export default function MemoryPage() {
+  return (
+    <AppShell>
+      <MemoryWorkspace />
+    </AppShell>
+  );
+}
